7eb1c733f02b70c5f3118630853360cd02c438ee
Patrick Huang <huangpatrick16777216@gmail.com>
2026-02-20 15:59:54 -0800

    Uart clock divider to center clock tick.

--- egcoding.v
+++ egcoding.v
@@ -4,4 +4,13 @@ module egcoding(
 	input wire rx,
 	input wire tx
 );
+	wire[7:0] rx_data;
+	wire rx_data_valid;
+	uart_rx rx_module(
+		.clk(clk),
+		.rst(rst),
+		.rx(rx),
+		.data(rx_data),
+		.data_valid(rx_data_valid)
+	);
 endmodule
--- uart_rx.v
+++ uart_rx.v
@@ -10,7 +10,7 @@ module uart_rx(
 	output reg data_valid
 );
 	// 2FF synchronizer.
-	reg rx_sync1, rx_sync2
+	reg rx_sync1, rx_sync2;
 
 	always @(posedge clk) begin
 		rx_sync1 <= rx;
@@ -21,43 +21,48 @@ module uart_rx(
 	// FSM states.
 	// Idle.
 	localparam S_IDLE = 2'd0;
-	// Received start bit. This state persists for the first half UART cycle to sync.
+	// Received start bit. This state persists for 1.5 UART cycles until first baud tick.
 	localparam S_START = 2'd1;
-	// Capturing data. Persists for 8 UART cycles.
+	// Capturing data. Persists for the remaining 7 UART cycles.
 	localparam S_DATA = 2'd2;
 	
 	reg[2:0] state;
 
 
 	// UART clock divider and syncer.
-	// Resp. for switching START -> DATA.
 	// 9600 baud = 5208 cycles per tick.
 	reg baud_tick;
 	reg[12:0] baud_clk_counter;
 
 	always @(posedge clk) begin
 		baud_tick <= 0;
-
-		case (state)
-		S_IDLE: begin
+		
+		if (state == S_IDLE) begin
 			baud_clk_counter <= 0;
-		end
-
-		S_START: begin
-			if (baud_clk_counter >= 2604) begin
-				state <= S_DATA;
-				baud_clk_counter <= 0;
-			end
-		end
-
-		S_DATA: begin
-			if (baud_clk_counter == 5208) begin
+		end else begin
+			baud_clk_counter <= baud_clk_counter + 1;
+			if ((state == S_START && baud_clk_counter >= 7812) ||
+				 (state == S_DATA && baud_clk_counter >= 5208)) begin
 				baud_tick <= 1;
 				baud_clk_counter <= 0;
 			end
-			baud_clk_counter <= baud_clk_counter + 1;
 		end
-		endcase
+	end
+
+	// Data capturer.
+	// Resp. for IDLE -> START and DATA -> IDLE.
+	always @(posedge clk) begin
+		data_valid <= 0;
+		// TODO DUMMY
+		data[0] <= baud_tick;
+
+		if (state == S_IDLE) begin
+			if (!rx)
+				state = S_START;
+		end else begin
+			if (baud_tick)
+				state = S_DATA;
+		end
 	end
 
 endmodule
